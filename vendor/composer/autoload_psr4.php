28b0b05576c55cdc721f777d02623066dd7878ce
Dev <devsanghavi53@gmail.com>
2025-01-02 00:15:22 +0530

    THIRD COMMIT

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Shsd\\Securepass\\' => array($baseDir . '/src'),
     'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
 );
